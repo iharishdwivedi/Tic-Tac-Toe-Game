6e37d9494d60613d5e7d74da2322b12b94317cb1
Harish Dwivedi <harishdwivedi346@gmail.com>
2025-06-09 09:33:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let resetbtn = document.querySelector('#reset')
 let turno=true;
 
 const winpatterns=[
-    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,6],[2,4,6],[3,4,5],[6,7,8]
+    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]
 ]
 
 const disableboxes=()=>{
